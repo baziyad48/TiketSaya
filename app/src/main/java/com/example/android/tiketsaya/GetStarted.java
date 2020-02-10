114d4d6a7da5085210c9c59d02850e46e17612e7
baziyad48 <huhummamalybaziyad@gmail.com>
2020-02-10 22:48:55 +0700

    add register activity.

--- app/src/main/java/com/example/android/tiketsaya/GetStarted.java
+++ app/src/main/java/com/example/android/tiketsaya/GetStarted.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class GetStarted extends AppCompatActivity {
-    Button btn_signIn;
+    Button btn_signIn, btn_getStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +16,21 @@ public class GetStarted extends AppCompatActivity {
         setContentView(R.layout.activity_get_started);
 
         btn_signIn = (Button) findViewById(R.id.btn_signIn);
+        btn_getStarted = (Button) findViewById(R.id.btn_getStarted);
 
         btn_signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(GetStarted.this, SignIn.class);
                 startActivity(intent);
-                finish();
+            }
+        });
+
+        btn_getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GetStarted.this, Register.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/android/tiketsaya/SignIn.java
+++ app/src/main/java/com/example/android/tiketsaya/SignIn.java
@@ -2,13 +2,28 @@ package com.example.android.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignIn extends AppCompatActivity {
+
+    TextView newAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        newAccount = (TextView) findViewById(R.id.btn_newAccount);
+
+        newAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignIn.this, Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
