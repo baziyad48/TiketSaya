80b29c210474b2b5e6cf78da91e0665c2e03e921
baziyad48 <huhummamalybaziyad@gmail.com>
2020-02-10 23:40:45 +0700

    add back button and intent.

--- app/src/main/java/com/example/android/tiketsaya/Register.java
+++ app/src/main/java/com/example/android/tiketsaya/Register.java
@@ -2,13 +2,28 @@ package com.example.android.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Register extends AppCompatActivity {
+
+    LinearLayout back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        back = (LinearLayout) findViewById(R.id.btn_back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
     }
 }
