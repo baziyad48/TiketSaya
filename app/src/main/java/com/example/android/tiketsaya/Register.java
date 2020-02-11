4a3f923b7c1d18aead9b6e8ee5c796325eeca827
baziyad48 <huhummamalybaziyad@gmail.com>
2020-02-11 16:02:22 +0700

    remove back button.

--- app/src/main/java/com/example/android/tiketsaya/Register.java
+++ app/src/main/java/com/example/android/tiketsaya/Register.java
@@ -9,21 +9,10 @@ import android.widget.LinearLayout;
 
 public class Register extends AppCompatActivity {
 
-    LinearLayout back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        back = (LinearLayout) findViewById(R.id.btn_back);
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Register.this, SignIn.class);
-                startActivity(intent);
-            }
-        });
     }
 }
