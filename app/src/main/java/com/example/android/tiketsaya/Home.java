f8fe73529b447d24c35188342a6ad1cd4ae9f285
baziyad48 <huhummamalybaziyad@gmail.com>
2020-02-11 20:59:51 +0700

    add ticket detail activity.

--- app/src/main/java/com/example/android/tiketsaya/Home.java
+++ app/src/main/java/com/example/android/tiketsaya/Home.java
@@ -2,13 +2,28 @@ package com.example.android.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
+
+    ImageView img_pisa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        img_pisa = (ImageView) findViewById(R.id.img_pisa);
+
+        img_pisa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, TicketDetail.class);
+                startActivity(intent);
+            }
+        });
     }
 }
