488d76fc006d501a0d1357ac76e221d6026ec306
baziyad48 <huhummamalybaziyad@gmail.com>
2020-02-11 19:09:45 +0700

    add home activity.

--- app/src/main/java/com/example/android/tiketsaya/RegisterSuccess.java
+++ app/src/main/java/com/example/android/tiketsaya/RegisterSuccess.java
@@ -2,13 +2,28 @@ package com.example.android.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterSuccess extends AppCompatActivity {
+
+    Button btn_explore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_success);
+
+        btn_explore = (Button) findViewById(R.id.btn_explore);
+
+        btn_explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterSuccess.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
