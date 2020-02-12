5445d12b35b4a80dd1c00d80dbdadba2c8732d62
baziyad48 <huhummamalybaziyad@gmail.com>
2020-02-12 22:44:18 +0700

    add profile activity.

--- app/src/main/java/com/example/android/tiketsaya/TicketSuccess.java
+++ app/src/main/java/com/example/android/tiketsaya/TicketSuccess.java
@@ -2,14 +2,37 @@ package com.example.android.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TicketSuccess extends AppCompatActivity {
 
+    Button btn_ticket, btn_dashboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_success);
+
+        btn_ticket = (Button) findViewById(R.id.btn_ticket);
+        btn_dashboard = (Button) findViewById(R.id.btn_dashboard);
+
+        btn_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TicketSuccess.this, Profile.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_dashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TicketSuccess.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
