28c1972b069d6cd7fd597872bf95e10189813b13
baziyad48 <huhummamalybaziyad@gmail.com>
2020-02-13 11:13:33 +0700

    add ticket and edit profile activity.

--- app/src/main/java/com/example/android/tiketsaya/Home.java
+++ app/src/main/java/com/example/android/tiketsaya/Home.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
 
-    ImageView img_pisa;
+    ImageView img_pisa, profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,20 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         img_pisa = (ImageView) findViewById(R.id.img_pisa);
+        profile = (ImageView) findViewById(R.id.profile);
 
         img_pisa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Home.this, TicketDetail.class);
+                startActivity(intent);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Profile.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/android/tiketsaya/Profile.java
+++ app/src/main/java/com/example/android/tiketsaya/Profile.java
@@ -2,13 +2,39 @@ package com.example.android.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Profile extends AppCompatActivity {
+
+    LinearLayout ticket_detail;
+    Button btn_edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ticket_detail = (LinearLayout) findViewById(R.id.ticket_detail);
+        btn_edit = (Button) findViewById(R.id.btn_edit);
+
+        ticket_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this, Ticket.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this, ProfileEdit.class);
+                startActivity(intent);
+            }
+        });
     }
 }
